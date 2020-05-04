04c29033f82078f71bb483e38bec65acb8dbab38
liurq123 <934905651@qq.com>
2020-05-04 10:53:52 +0800

    修改了推荐界面

--- src/containers/recommend/recommend.jsx
+++ src/containers/recommend/recommend.jsx
@@ -5,7 +5,7 @@ class Recommend extends Component{
     render() {
         return(
             <div>
-                <h2>我是推荐页面</h2>
+                <h2>我是推荐页面，对啊对啊</h2>
             </div>
         )
     }
